8d15f6262fd5c55d14e76e6d53c5c1473f834f73
abrsh12 <abrahamelefachew@gmail.com>
2025-08-04 15:00:58 +0300

    verify email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,47 +1,56 @@
-import { Body, Controller, ForbiddenException, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dtos/signup.dto';
 import { ForgotPasswordDTO, LoginDto, VerifyEmailDto } from './dtos/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
-        // post signup
-        @Post('signup')
-        async signup(@Body() SignUpData: SignupDto) {
-            return this.authService.signup(SignUpData);
-        }
+  constructor(private readonly authService: AuthService) {}
+  // post signup
+  @Post('signup')
+  async signup(@Body() signUpData: SignupDto) {
+    return this.authService.signup(signUpData);
+  }
 
-        // post login
-        @Post('login')
-        async login(@Body() LoginData: LoginDto) {
-            return this.authService.login(LoginData);
-        }
-        // post Forgot Password
-        @Post('forgot-password')
-        async forgotPassword(@Body() dto: ForgotPasswordDTO) {
-            return this.authService.forgotPassword(dto.email);
-        }
-        // post Reset Password
-        @Post('reset-password')
-        async resetPassword(
-            @Query('token') token: string,
-            @Body('password') password: string,
-        ) {
-            return this.authService.resetPassword(token, password);
-        }
+  // post login
+  @Post('login')
+  async login(@Body() LoginData: LoginDto) {
+    return this.authService.login(LoginData);
+  }
+  // post Forgot Password
+  @Post('forgot-password')
+  async forgotPassword(@Body() dto: ForgotPasswordDTO) {
+    return this.authService.forgotPassword(dto.email);
+  }
+  // post Reset Password
+  @Post('reset-password')
+  async resetPassword(
+    @Query('token') token: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.resetPassword(token, password);
+  }
 
-        // post Verify Email
-        @Post('verify-email')
-        async verifyEmail(@Body() VerifyData: VerifyEmailDto) {
-            try {
-            return await this.authService.verifyEmail(VerifyData.email, VerifyData.otp);
-            } catch (error) {
-            console.log(error);
-            throw new ForbiddenException('Unable to verify');
-            }
-        }
-        // post Social Login
-        // post Social Signup
-        
+  // post Verify Email
+  @Post('verify-email')
+  async verifyEmail(@Body() VerifyData: VerifyEmailDto) {
+    try {
+      return await this.authService.verifyEmail(
+        VerifyData.email,
+        VerifyData.otp,
+      );
+    } catch (error) {
+      console.log(error);
+      throw new ForbiddenException('Unable to verify');
+    }
+  }
+  // post Social Login
+  // post Social Signup
 }
