db248f04c7dfd2a2f4740310b7e974ec49df3fb3
abrsh12 <abrahamelefachew@gmail.com>
2025-08-27 11:56:04 +0300

    password criteria updated

--- src/auth/dtos/signup.dto.ts
+++ src/auth/dtos/signup.dto.ts
@@ -1,27 +1,30 @@
 import { IsEmail, IsEnum, IsOptional, IsPhoneNumber, IsString, Matches } from "class-validator";
 import { UserRole } from "src/user/user.model";
 
-export class SignupDto{
-    @IsString()
-    name: string;
+export class SignupDto {
+  @IsString()
+  name: string;
 
-    @IsEmail()
-    email: string;
+  @IsEmail({}, { message: 'Invalid email format.' })
+  email: string;
 
-    @IsString()
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&#]{6,}$/, {
-        message: 'Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, and one number.',
-    })
-    password: string;
-    @IsOptional()
-    @IsPhoneNumber('ET', { message: 'Phone number must be a valid Ethiopian phone number.' })
-    phone?: string;
-   
-    @IsOptional()
-    @IsString()
-    address?: string;
+  @IsString()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&#]{6,}$/, {
+    message:
+      'Password must be at least 6 characters long and include both letters and numbers. Symbols are optional.',
+  })
+  password: string;
+  @IsOptional()
+  @IsPhoneNumber('ET', {
+    message: 'Phone number must be a valid Ethiopian phone number.',
+  })
+  phone?: string;
 
-    // @IsOptional()
-    // @IsEnum(UserRole, { message: 'Role must be either ADMIN or CUSTOMER.' } )
-    // role?: UserRole = UserRole.CUSTOMER; // Default to CUSTOMER if not provided
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  // @IsOptional()
+  // @IsEnum(UserRole, { message: 'Role must be either ADMIN or CUSTOMER.' } )
+  // role?: UserRole = UserRole.CUSTOMER; // Default to CUSTOMER if not provided
 }
